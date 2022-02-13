0741b3e698e69b8ad5d189889fe493d25db35317
kalyan mishra <55200919+thekalyan001@users.noreply.github.com>
2022-02-13 15:24:07 +0530

    Update 04  Find first and last positions of an element in a sorted array (Medium).cpp

--- 2. Search & Sort/04  Find first and last positions of an element in a sorted array (Medium).cpp
+++ 2. Search & Sort/04  Find first and last positions of an element in a sorted array (Medium).cpp
@@ -1,28 +1,41 @@
-	vector<int> alternateSort(int arr[], int n)
-	{
-	   // sort(arr,arr+n);int p=0;
-	   // vector<int>v(n);
-	   // for(int i=0;i<=n/2;i++)
-	   // {
+////https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+	// vector<int> alternateSort(int arr[], int n)
+	// {
+	//    sort(arr,arr+n);int p=0;
+	//    vector<int>v(n);
+	//    for(int i=0;i<=n/2;i++)
+	//    {
 	
-	   //     v[p++]=arr[n-i-1];
-	   //     p+1<n?v[p++]=arr[i]:0;
-	   // }
-	   // return v;
-	   
-	   
-	   vector<int>v(n),i=0;
-	   priority_queue<int>mnH;
-	   priority_queue<int,vector<int>,greater<int>>mxH;
-	   for(int i=0;i<n;i++)
-	   {
-	       mnH.push(arr[i]);
-	       mxH.push(arr[i]);
-	   }
-	    while(mnH.top()!=mxH.top())
-	    {
-	        v[i++]=mxH.top();mxH.pop();
-	        v[i++]=mnH.top();mnH.pop();
-	    }
-	    //take count variable and if <n then work
-	}
+	//        v[p++]=arr[n-i-1];
+	//        p+1<n?v[p++]=arr[i]:0;
+	//    }
+	//    return v;
+	// }
+
+
+ vector<int> searchRange(vector<int>& nums, int target) {
+        
+       int start=0,end=nums.size()-1,mid,left,right;
+       while(start<=end){
+           mid=(start+end)/2;
+           if(nums[mid]>=target)
+               end=mid-1;
+           else
+               start=mid+1;
+       }
+        
+        left=start;
+        
+        start=0,end=nums.size()-1;
+        while(start<=end){
+              mid = (start + end) / 2;
+        if (nums[mid] > target)
+            end = mid-1;
+        else
+            start = mid + 1;
+        }
+        right=start;
+        
+      return left == right ? vector<int> {-1,-1} : vector<int> {left,right-1};
+    }
